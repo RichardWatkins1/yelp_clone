648e492e1d67a054acb6db7f8d11cdbc2b31af44
RichardWatkins1 <richardwatkins1@gmail.com>
2016-05-25 12:53:35 +0100

    facebook sign in worked, doesnt show sign out button, normal sign up does

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ end
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.6'
 gem 'devise'
+gem 'omniauth-facebook'
 # Use postgresql as the database for Active Record
 gem 'pg', '~> 0.15'
 # Use SCSS for stylesheets
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,6 +14,7 @@ Devise.setup do |config|
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
 
+  config.omniauth :facebook, 438111603029301, Rails.application.secrets.facebook_app_secret
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
