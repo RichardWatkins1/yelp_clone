df3d27db9bf5be04bd20da50edcbdc566072df74
MariaRomero <fantasysystems@gmail.com>
2016-05-26 15:38:25 +0100

    finish v_3

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,10 @@
 class Restaurant < ActiveRecord::Base
 
-  has_many :reviews
+  # has_many :reviews
   validates :name, length: { minimum: 3 }
+
+  has_many :reviews,
+      -> { extending WithUserAssociationExtension },
+      dependent: :destroy
 
 end
